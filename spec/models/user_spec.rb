9d30ae230a5d3b98d586a17cf60222f346803a6b
Addisu Haile Tedla <addisu.haile@yahoo.com>
2022-10-12 16:57:12 +0300

    Modify the navbar

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,8 +5,12 @@ RSpec.describe User, type: :model do
     @user = User.new(name: 'Addisu', email: 'addisu@gmail.com', password: '123456', password_confirmation: '123456')
   end
 
-  it 'user should be valid' do 
+  it 'user should be valid' do
     expect(@user).to be_valid
+  end
+
+  it 'validates the user name match' do
+    expect(@user.name).to eql('Addisu')
   end
 
   it 'name should be present' do
@@ -19,8 +23,16 @@ RSpec.describe User, type: :model do
     expect(@user).to_not be_valid
   end
 
+  it 'validates the user email match' do
+    expect(@user.email).to eql('addisu@gmail.com')
+  end
+
   it 'password should be present' do
     @user.password = nil
     expect(@user).to_not be_valid
+  end
+
+  it 'validates the user password match' do
+    expect(@user.password).to eql('123456')
   end
 end
