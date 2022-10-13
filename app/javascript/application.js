5ed4a6cceaae066f96d6767387d9b6b5dcda02f0
Addisu Haile Tedla <addisu.haile@yahoo.com>
2022-10-13 17:39:14 +0300

    Update the functionality of the navbar

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,6 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import '@hotwired/turbo-rails';
 import 'controllers';
+import 'flowbite';
+import { Turbo } from '@hotwired/turbo-rails';
+Turbo.session.drive = false;
