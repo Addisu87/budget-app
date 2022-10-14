6bdd388d995ca32c7f47eece848c1441637d8972
Addisu Haile Tedla <addisu.haile@yahoo.com>
2022-10-14 17:52:28 +0300

    Update my config/database.yml file

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,13 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2022_10_09_063819) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "categories", force: :cascade do |t|
     t.string "name"
     t.string "icon"
-    t.integer "author_id", null: false
+    t.bigint "author_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["author_id"], name: "index_categories_on_author_id"
@@ -23,8 +26,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_09_063819) do
   create_table "purchases", force: :cascade do |t|
     t.string "name"
     t.float "amount"
-    t.integer "author_id", null: false
-    t.integer "category_id", null: false
+    t.bigint "author_id", null: false
+    t.bigint "category_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["author_id"], name: "index_purchases_on_author_id"
