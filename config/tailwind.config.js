f61c6480ff99d9ee41a8aebd9b6604528d2e2b51
Addisu Haile Tedla <addisu.haile@yahoo.com>
2022-10-14 18:05:29 +0300

    Update the flowbite functionality and fix the functionality of the delete button

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -21,7 +21,6 @@ module.exports = {
   plugins: [
     require('@tailwindcss/forms'),
     require('@tailwindcss/aspect-ratio'),
-    require('@tailwindcss/typography'),
-    require('flowbite/plugin')
+    require('@tailwindcss/typography')
   ]
 };
