dd908fabfcfc6735a87a6b4a60335a8f986d2453
Addisu <addisuhaile87@gmail.com>
2022-12-21 00:04:55 +0300

    Remove forgery syntax

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,4 @@
-class ApplicationController < ActionController::API
-  # protect_from_forgery with: :exception, unless: -> { request.format.json? }
+class ApplicationController < ActionController::Base
   before_action :authenticate_user!
 
   before_action :configure_permitted_parameters, if: :devise_controller?
