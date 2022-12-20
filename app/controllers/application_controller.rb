595ecad7bd38b07da106553f5688c8f2b83e0d9d
Addisu <addisuhaile87@gmail.com>
2022-12-20 23:50:25 +0300

    Update the application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,4 @@
-class ApplicationController < ActionController::Base
+class ApplicationController < ActionController::API
   protect_from_forgery with: :exception, unless: -> { request.format.json? }
   before_action :authenticate_user!
 
