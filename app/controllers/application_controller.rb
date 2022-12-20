11fb79f1caccef58b7f0218c4849289f42a01a79
Addisu <addisuhaile87@gmail.com>
2022-12-20 23:55:44 +0300

    Comment the forgery message

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::API
-  protect_from_forgery with: :exception, unless: -> { request.format.json? }
+  # protect_from_forgery with: :exception, unless: -> { request.format.json? }
   before_action :authenticate_user!
 
   before_action :configure_permitted_parameters, if: :devise_controller?
